d756a104f2b996ba46e395af5cea6629b57151c2
datapolicykr <datapolicy.kr@gmail.com>
2026-09-09 16:23:11 +0900

    style: load PPT reference overrides for VAN survey site

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import "./ppt-reference.css";
 import "./admin-position.css";
 import DashboardAdmin from "./DashboardAdmin";
 import ComparisonDirection from "./ComparisonDirection";
